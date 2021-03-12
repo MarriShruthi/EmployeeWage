25558b65ca56ab7bda19b6b31d7dca000d474a74
Shruthi <shruthimarri97@gmail.com>
2021-03-12 19:50:16 +0530

    [Shruthi] Add : Employee Wages till condition of Hrs or Days reached implemented

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -7,14 +7,15 @@ namespace EmployeeWage
         static void Main(string[] args)
 		{
 			Console.WriteLine("welcome to employee Program");
-			const int EmpWagePerHr = 20, Num_Of_Working_Days = 20;
+			const int EmpWagePerHr = 20, Num_Of_Working_Days = 20, Max_Working_Hrs = 100;
 			const int Is_FullTime = 1, Is_PartTime = 2;
-			int EmpHrs=0, EmpWage = 0, TotalWages = 0;
-			for (int day = 0; day <= Num_Of_Working_Days; day++)
+			int EmpHrs = 0, EmpWage = 0, TotalWages = 0, TotalHrs = 0, WorkingDays = 0;
+			while (TotalHrs <= Max_Working_Hrs && WorkingDays < Num_Of_Working_Days)
 			{
+				Console.WriteLine("Day :" + WorkingDays);
+				WorkingDays++;
 				Random random = new Random();
 				int empCheck = random.Next(1, 3);
-				Console.WriteLine("day :" + day);
 				switch (empCheck)
 				{
 					case Is_FullTime:
@@ -32,7 +33,9 @@ namespace EmployeeWage
 				}
 				EmpWage = EmpHrs * EmpWagePerHr;
 				Console.WriteLine("Employee Wage :" + EmpWage);
+				TotalHrs = TotalHrs + EmpHrs;
 				TotalWages = TotalWages + EmpWage;
+
 			}
 			Console.WriteLine("Total Monthly Emp Wages = " + TotalWages);
 		}
