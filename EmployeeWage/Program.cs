8b7931b606ebc226e3f62046f345f9dcb2c5551c
Shruthi <shruthimarri97@gmail.com>
2021-03-12 19:05:02 +0530

    [Shruthi] Add : Employee Wage in Switch Case Statement implemented

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -4,18 +4,31 @@ namespace EmployeeWage
 {
 	class Program1
 	{
-		static void Main(string[] args)
+        static void Main(string[] args)
 		{
 			Console.WriteLine("welcome to employee Program");
-			int isPartTime = 1;
-			int EmpWagePerHr = 20;
+			const int EmpWagePerHr = 20;
+			int EmpHrs;
+			const int Is_FullTime = 1, Is_PartTime = 2;
 			Random random = new Random();
-			int empCheck = random.Next(0, 2);
-			if (empCheck == isPartTime)
-				Console.WriteLine("Employee is Part Time");
-				int EmpHrs = 8;
-				int EmpWage =(EmpHrs * EmpWagePerHr);
-				Console.WriteLine("Emp Wage = "+EmpWage);
+			int empCheck = random.Next(1,3);
+            switch (empCheck)
+			{
+				case Is_FullTime:
+					EmpHrs = 8;
+					Console.WriteLine("Employee is Full Time");
+					break;
+				case Is_PartTime:
+                    EmpHrs = 4;
+					Console.WriteLine("Employee is Part Time");
+					break;
+				default :
+					EmpHrs = 0;
+					Console.WriteLine("Default Value");
+					break;
+			}
+			int EmpWage = EmpHrs * EmpWagePerHr;
+			Console.WriteLine("Emp Wage =" + EmpWage);
 		}
 	}
 }
