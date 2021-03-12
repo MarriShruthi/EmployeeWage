f78564175aad29439bd9208d8ad8fd65ef5477a7
Shruthi <shruthimarri97@gmail.com>
2021-03-12 10:53:29 +0530

    [Shruthi] Add : Employee Wage Attendence Feature implemented

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -7,6 +7,13 @@ namespace EmployeeWage
 		static void Main(string[] args)
 		{
 			Console.WriteLine("welcome to employee Program");
+			int isPresent = 1;
+			Random random = new Random();
+			int empCheck = random.Next(0, 2);
+			if (empCheck == isPresent)
+				Console.WriteLine("Employee is Present");
+			else
+				Console.WriteLine("Employee is Absent");
 		}
 	}
 }
