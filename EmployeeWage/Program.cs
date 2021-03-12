cd77e7dc07fdc1782dd382e2321e52c08aa323b3
Shruthi <shruthimarri97@gmail.com>
2021-03-12 15:36:55 +0530

    [Shruthi] Add : Employee Daily Wage Feature implemented

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -8,12 +8,15 @@ namespace EmployeeWage
 		{
 			Console.WriteLine("welcome to employee Program");
 			int isPresent = 1;
+			int EmpWagePerHr = 20;
 			Random random = new Random();
 			int empCheck = random.Next(0, 2);
 			if (empCheck == isPresent)
 				Console.WriteLine("Employee is Present");
-			else
-				Console.WriteLine("Employee is Absent");
+				int EmpHrs = 8;
+				int EmpWage =(EmpHrs * EmpWagePerHr);
+				Console.WriteLine("Emp Wage = "+EmpWage);
 		}
 	}
 }
+
