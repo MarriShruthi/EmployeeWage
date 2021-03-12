25db32a6cb8088fc42e5332804fea89fd5f8be3f
Shruthi <shruthimarri97@gmail.com>
2021-03-12 15:53:04 +0530

    [Shruthi] Add : Employee Part Time Wage Feature implemented

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -7,12 +7,12 @@ namespace EmployeeWage
 		static void Main(string[] args)
 		{
 			Console.WriteLine("welcome to employee Program");
-			int isPresent = 1;
+			int isPartTime = 1;
 			int EmpWagePerHr = 20;
 			Random random = new Random();
 			int empCheck = random.Next(0, 2);
-			if (empCheck == isPresent)
-				Console.WriteLine("Employee is Present");
+			if (empCheck == isPartTime)
+				Console.WriteLine("Employee is Part Time");
 				int EmpHrs = 8;
 				int EmpWage =(EmpHrs * EmpWagePerHr);
 				Console.WriteLine("Emp Wage = "+EmpWage);
