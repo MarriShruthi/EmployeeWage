edb3442e6fe905fa6f5746d0847996a7fa28145f
Shruthi <shruthimarri97@gmail.com>
2021-03-12 19:33:49 +0530

    [Shruthi] Add : Employee monthly Wages  implemented

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -7,28 +7,34 @@ namespace EmployeeWage
         static void Main(string[] args)
 		{
 			Console.WriteLine("welcome to employee Program");
-			const int EmpWagePerHr = 20;
-			int EmpHrs;
+			const int EmpWagePerHr = 20, Num_Of_Working_Days = 20;
 			const int Is_FullTime = 1, Is_PartTime = 2;
-			Random random = new Random();
-			int empCheck = random.Next(1,3);
-            switch (empCheck)
+			int EmpHrs=0, EmpWage = 0, TotalWages = 0;
+			for (int day = 0; day <= Num_Of_Working_Days; day++)
 			{
-				case Is_FullTime:
-					EmpHrs = 8;
-					Console.WriteLine("Employee is Full Time");
-					break;
-				case Is_PartTime:
-                    EmpHrs = 4;
-					Console.WriteLine("Employee is Part Time");
-					break;
-				default :
-					EmpHrs = 0;
-					Console.WriteLine("Default Value");
-					break;
+				Random random = new Random();
+				int empCheck = random.Next(1, 3);
+				Console.WriteLine("day :" + day);
+				switch (empCheck)
+				{
+					case Is_FullTime:
+						EmpHrs = 8;
+						Console.WriteLine("Employee is Full Time");
+						break;
+					case Is_PartTime:
+						EmpHrs = 4;
+						Console.WriteLine("Employee is Part Time");
+						break;
+					default:
+						EmpHrs = 0;
+						Console.WriteLine("Default Value");
+						break;
+				}
+				EmpWage = EmpHrs * EmpWagePerHr;
+				Console.WriteLine("Employee Wage :" + EmpWage);
+				TotalWages = TotalWages + EmpWage;
 			}
-			int EmpWage = EmpHrs * EmpWagePerHr;
-			Console.WriteLine("Emp Wage =" + EmpWage);
+			Console.WriteLine("Total Monthly Emp Wages = " + TotalWages);
 		}
 	}
 }
