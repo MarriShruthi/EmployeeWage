42cd7c3e07f7cd198092feb1938552d5436284e4
Shruthi <shruthimarri97@gmail.com>
2021-04-20 15:53:54 +0530

    [Shruthi] : Added EmpWAGE OF Multiple Companies using interface

---
+++ EmployeeWage/IComputeEmpWage.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployeeWage
+{
+    internal interface IComputeEmpWage
+    {
+        void AddCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth);
+        void computeEmpWage();
+        int getTotalWage(string company);
+    }
+}
