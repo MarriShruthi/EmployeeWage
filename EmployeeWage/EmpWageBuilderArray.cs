3a2d7acd976871566f8f7c46b71c404696aab59c
Shruthi <shruthimarri97@gmail.com>
2021-04-20 16:00:50 +0530

    [Shruthi] : Added EmpWAGE OF Multiple Companies using interface

--- EmployeeWage/EmpWageBuilderArray.cs
+++ EmployeeWage/EmpWageBuilderArray.cs
@@ -10,34 +10,35 @@ namespace EmployeeWage
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
 
-        private LinkedList<CompanyEmpWage> companyEmpWaeList;
+        private int numOfCompany = 0;
+        private CompanyEmpWage[] companyEmpWageArray;
         private Dictionary<string, CompanyEmpWage> companyToEmpWageMap;
 
         public EmpWageBuilderArray() //constructor
         {
-            this.companyEmpWaeList = new LinkedList<CompanyEmpWage>();
+            this.companyEmpWageArray = new CompanyEmpWage[5];
             this.companyToEmpWageMap = new Dictionary<string, CompanyEmpWage>();
 
         }
 
         public void AddCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHourPerMonth)
         {//body of interface member
+            companyEmpWageArray[this.numOfCompany] = new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays, maxHourPerMonth);
             CompanyEmpWage companyEmpWage = new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays, maxHourPerMonth);
-            this.companyEmpWaeList.AddLast(companyEmpWage);
-            this.companyToEmpWageMap.Add(company, companyEmpWage);
-
+            companyToEmpWageMap.Add(company, companyEmpWage);
+            numOfCompany++;
         }
         public void computeEmpWage() //body of interface member
         {
-            foreach (CompanyEmpWage companyEmpWage in this.companyEmpWaeList)
+            for (int i = 0; i < numOfCompany; i++)
             {
-                companyEmpWage.setTotalEmpWage(this.computeEmpWage(companyEmpWage));
-                Console.WriteLine(companyEmpWage.toString());
+                companyEmpWageArray[i].setTotalEmpWage(this.computeEmpWage(this.companyEmpWageArray[i]));
+                Console.WriteLine(this.companyEmpWageArray[i].toString());
             }
         }
         public int computeEmpWage(CompanyEmpWage companyEmpWage)
         {
-            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0; //variables
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
             while (totalEmpHrs <= companyEmpWage.maxHoursPerMonth && totalWorkingDays < companyEmpWage.numOfWorkingDays)
             {
                 totalWorkingDays++;
@@ -62,7 +63,10 @@ namespace EmployeeWage
         }
         public int getTotalWage(string company) //using get method
         {
-            return this.companyToEmpWageMap[company].totalEmpWage;
+            Console.WriteLine("Inside GetTotalWage Method");
+            int result = this.companyToEmpWageMap[company].totalEmpWage;
+            Console.WriteLine("TotalWage is : {0}", result);
+            return result;
         }
     }
 }
--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -11,7 +11,6 @@ namespace EmployeeWage
 			empWageBuilder.AddCompanyEmpWage("DMart", 20, 2, 10); //call AddCompany method
 			empWageBuilder.AddCompanyEmpWage("Reliance", 10, 4, 20);
 			empWageBuilder.computeEmpWage();
-			Console.WriteLine("Total Wage for DMart Comapny " + empWageBuilder.getTotalWage("DMart"));
 			Console.Read();
 		}
 	}
